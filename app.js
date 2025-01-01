0ade2b584c6a7eaa6d1373a292e609255d2209bf
Curren Taber <59625593+currenluna@users.noreply.github.com>
2025-01-01 13:17:11 -0800

    Add page transition

--- app.js
+++ app.js
@@ -8,12 +8,20 @@ window.Webflow.push(() => {
 
 let nav_open = false;
 
- // use a script tag or an external JS file
- document.addEventListener("DOMContentLoaded", (event) => {
+// use a script tag or an external JS file
+document.addEventListener("DOMContentLoaded", (event) => {
     const nav_open_wrap = document.querySelector(".nav_open_wrap");
     const nav_open_icon = document.querySelector(".nav_open_icon");
     const nav_layout = document.querySelector(".nav_layout");
-    const tl = gsap
+
+    const tl_load = gsap
+        .timeline({ paused: false })
+        .from(".page_main",
+            { y: 32, opacity: 0, duration: 0.5, delay: 0.2, ease: "power3.ut"})
+        .from(".nav_wrap",
+            { scale: 0.9, opacity: 0, duration: 0.7, ease: "power3.out" }, "<");
+
+    const tl_nav = gsap
         .timeline({ paused: true })
         .fromTo(nav_layout,
             { height: "64px" },
@@ -22,17 +30,20 @@ let nav_open = false;
             { rotation: 0 },
             { rotation: -45, duration: 0.5, ease: "power3.inOut"}, "<");
 
+    tl_load.play();
+    console.log("here");
+
     nav_open_wrap.addEventListener("click", () => {
         if (!nav_open) {
-            tl.play();
+            tl_nav.play();
             nav_open = true;
         } else {
-            tl.reverse();
+            tl_nav.reverse();
             nav_open = false;
         }
     });
 
     window.addEventListener("resize", () => {
-        tl.invalidate();
+        tl_nav.invalidate();
     });
 });
---
+++ slider.js
@@ -0,0 +1,71 @@
+window.Webflow ||= [];
+window.Webflow.push(() => {
+    //alert("hello world hi");
+});
+
+
+// use a script tag or an external JS file
+document.addEventListener("DOMContentLoaded", (event) => {
+
+
+    $(".slider_wrap").each(function (index) {
+        let loopMode = false;
+        if ($(this).attr("loop-mode") === "true") {
+            loopMode = true;
+        }
+        let sliderDuration = 300;
+        if ($(this).attr("slider-duration") !== undefined) {
+            sliderDuration = +$(this).attr("slider-duration");
+        }
+        const swiper = new Swiper($(this).find(".swiper")[0], {
+            speed: sliderDuration,
+            loop: loopMode,
+            autoHeight: false,
+            centeredSlides: loopMode,
+            followFinger: true,
+            freeMode: false,
+            slideToClickedSlide: false,
+            slidesPerView: "auto",
+            spaceBetween: 16,
+            rewind: false,
+            mousewheel: {
+            forceToAxis: true
+            },
+            keyboard: {
+            enabled: true,
+            onlyInViewport: true
+            },
+            breakpoints: {
+            // mobile landscape
+            480: {
+                slidesPerView: "auto",
+                spaceBetween: 16
+            },
+            // tablet
+            768: {
+                slidesPerView: "auto",
+                spaceBetween: 16
+            },
+            // desktop
+            992: {
+                slidesPerView: "auto",
+                spaceBetween: 16
+            }
+            },
+            pagination: {
+            el: $(this).find(".swiper-bullet-wrapper")[0],
+            bulletActiveClass: "is-active",
+            bulletClass: "swiper-bullet",
+            bulletElement: "button",
+            clickable: true
+            },
+            navigation: {
+            nextEl: $(this).find(".swiper-next")[0],
+            prevEl: $(this).find(".swiper-prev")[0],
+            disabledClass: "is-disabled"
+            },
+            slideActiveClass: "is-active",
+            slideDuplicateActiveClass: "is-active"
+        });
+    });
+});
